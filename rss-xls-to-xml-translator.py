e065dbb7df7b6adeffa05029f29bc56ee77bb5da
Vasco Nunes <vbnunes@criticalsoftware.com>
2018-10-16 17:44:49 +0100

    still fixing issue #9

--- rss-xls-to-xml-translator.py
+++ rss-xls-to-xml-translator.py
@@ -590,16 +590,16 @@ if __name__ == '__main__':
         nfiledata = re.sub('%%.*?%%', '', nfiledata)
         nfiledata = nfiledata.replace("<gco:Date></gco:Date>","")
         nfiledata = nfiledata.replace("> &gt; ",">  &gt; ").replace("  &gt; ","").replace(" &gt; <","<").replace(" &gt;<","<")
-        # Encode urls
-        # urls = re.findall('"http.?.*?"', nfiledata)
-        # for url in urls:
-        #     if url.find("gmd")<=0 and url.find("3.2")<=0 and url.find("isotc211")<=0 and url.find("w3.org")<=0:
-        #         # encodedurl = quote(url.replace('\"',''), safe='')
-        #         # nfiledata = nfiledata.replace(url, '\"' + encodedurl + '\"')
-        #         cleanurl = urllib.parse.unquote(url)
-        #         encodedurl = cleanurl.replace('&','&amp;')
-        #         nfiledata = nfiledata.replace(url, encodedurl)
-        nfiledata = nfiledata.replace('&','&amp;')
+        # Decode urls
+        urls = re.findall('"http.?.*?"', nfiledata)
+        for url in urls:
+            if url.find("gmd")<=0 and url.find("3.2")<=0 and url.find("isotc211")<=0 and url.find("w3.org")<=0:
+                # encodedurl = quote(url.replace('\"',''), safe='')
+                # nfiledata = nfiledata.replace(url, '\"' + encodedurl + '\"')
+                cleanurl = urllib.parse.unquote(url)
+                encodedurl = cleanurl.replace('&','&amp;')
+                nfiledata = nfiledata.replace(url, encodedurl)
+        nfiledata = re.sub('&(?!amp;|gt;|lt;)', '&amp;', nfiledata)
         # Remove specific empty tags
         tags = ['gmi:platform','gmi:instrument']
         for tag in tags:
